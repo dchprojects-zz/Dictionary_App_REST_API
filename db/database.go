ace231ed1033a81bbe5eb4cb324d7703adf97362
Dima <dima.chumakovwork@gmail.com>
2021-07-14 19:29:57 +0300

    added host, port

--- db/database.go
+++ db/database.go
@@ -8,14 +8,16 @@ import (
 
 func CreateDatabase() (*sql.DB, error) {
 
-	user := "d9d9vs9"
-	password := "Vagina$2020"
-	dbName := "dbword"
+	host := "50.87.139.17"
+	port := "3306"
+	user := "dchproje_N0"
+	password := "Future$Projects@2021"
+	dbName := "dchproje_dbmdwords"
 
-	db, err := sql.Open("mysql", user+":"+password+"@/"+dbName)
+	db, err := sql.Open("mysql", user+":"+password+"@tcp"+"("+host+":"+port+")"+"/"+dbName)
 	if err != nil {
 		return nil, err
 	}
-
+	
 	return db, nil
 }
