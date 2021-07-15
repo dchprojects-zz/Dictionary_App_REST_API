8e2c09bbf1ff760ad020dbe62826a5a0b1445f36
Dima <dima.chumakovwork@gmail.com>
2021-07-15 12:13:46 +0300

    added .env, go echo api

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module goproj
 go 1.15
 
 require (
-	github.com/gorilla/mux v1.8.0
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 )
--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	"goproj/app"
-	"goproj/db"
-	"log"
+	"fmt"
 	"net/http"
+	"os"
+	"path"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	database, err := db.CreateDatabase()
-	if err != nil {
-		log.Fatal("Database connection failed: %s", err.Error())
-	}
 
-	app := &app.App{
-		Router:   mux.NewRouter().StrictSlash(true),
-		Database: database,
-	}
+	server := echo.New()
+	server.GET(path.Join("/"), Version)
 
-	app.SetupRouter()
+	godotenv.Load()
+	port := os.Getenv("PORT")
 
-	log.Fatal(http.ListenAndServe(":8000", app.Router))
+	address := fmt.Sprintf("%s:%s", "0.0.0.0", port)
+
+	server.Start(address)
+	// database, err := db.CreateDatabase()
+	// if err != nil {
+	// 	log.Fatal("Database connection failed: %s", err.Error())
+	// }
+
+	// app := &app.App{
+	// 	Router:   mux.NewRouter().StrictSlash(true),
+	// 	Database: database,
+	// }
+
+	// app.SetupRouter()
+
+}
+
+func Version(context echo.Context) error {
+	return context.JSON(http.StatusOK, map[string]interface{}{"version": 1})
 }
